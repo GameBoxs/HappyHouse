ce110dc11f91e4d0efcabd9e76b640404c65b79a
김지훈 <ji_hun23@naver.com>
2022-11-16 17:54:10 +0900

    feat(FE): create FindApt Title

--- FrontEnd/src/components/NavBar.vue
+++ FrontEnd/src/components/NavBar.vue
@@ -19,9 +19,19 @@
             <li class="nav-item"><a class="nav-link" href="#portfolio"><span class="navspan">Q&A</span></a></li>
             <li class="nav-item"><a class="nav-link" href="#about"><span class="navspan">아파트 매매 찾기</span></a></li>
             <li class="nav-item"><a class="nav-link" href="#team"><span class="navspan">오늘의 뉴스</span></a></li>
+            <li class="nav-item">
+              <a href="${root}/member?act=mvregist" class="btn btn-default btn-sm text-white">
+                <i class="fa fs-5 fa-user pr-2 fa-beat-fade" style="--fa-animation-duration: 2s;"></i> <span class="navspan">SignUp</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{name:'login'}" class="btn btn-default btn-sm text-white">
+                <i class="fa fs-5 fa-lock pr-2 fa-shake" style="--fa-animation-duration: 2s;"></i> <span class="navspan">Login</span>
+              </router-link>
+            </li>
           </ul>
         </div>
-        <div class="ms-5">
+        <!-- <div class="ms-5">
           <div class="btn-group">
             <a href="${root}/member?act=mvregist" class="btn btn-default btn-sm text-white"><i class="fa fs-5 fa-user pr-2 fa-beat-fade" style="--fa-animation-duration: 2s;"></i> <span class="navspan">SignUp</span></a>
           </div>
@@ -29,45 +39,7 @@
             <router-link :to="{name:'login'}" class="btn btn-default btn-sm text-white">
               <i class="fa fs-5 fa-lock pr-2 fa-shake" style="--fa-animation-duration: 2s;"></i> <span class="navspan">Login</span>
             </router-link>
-            <!-- <button type="button" class="btn btn-default btn-sm text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">
-              <i class="fa fs-5 fa-lock pr-2 fa-shake" style="--fa-animation-duration: 2s;"></i> <span class="navspan">Login</span>
-            </button> -->
-            <!-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-body">
-                    <section>
-                      <div class="container">
-                        <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                          <div class="card-body p-5 text-center">
-                            <div class="">
-                              <form method="post" action="${root}/member">
-                                <input type="hidden" name="act" value="login">
-                                <h2 class="fw-bold text-white mb-2 text-uppercase">Login</h2>
-                                <p class="text-white-50 mb-5">Please enter your login and password!</p>
-                                <div class="form-outline form-white mb-4">
-                                  <label class="form-label" for="id">ID</label>
-                                  <input type="text" id="nid" name="id" class="form-control form-control-lg" />
-                                </div>
-                                <div class="form-outline form-white mb-4">
-                                  <label class="form-label" for="password">Password</label>
-                                  <input type="password" id="password" name="password" class="form-control form-control-lg" />
-                                </div>
-                                <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-                              </form>
-                            </div>
-                            <div class="pt-5">
-                                <p class="mb-0">Don't have an account? <a href="${root}/member?act=mvregist" class="text-white-50 fw-bold">SignUp</a></p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </section>
-                  </div>
-                </div>
-              </div>
-            </div> -->
-          </div>
+          </div> -->
           <!-- <div class="top-bar text-white-50 row gx-0 align-items-center d-none d-lg-flex" style="display: none;">
             <div class="col-3 col-sm-6 col-lg-9"></div>
             <div class="col-9 col-sm-6 col-lg-3">
@@ -90,7 +62,7 @@
               </div>
             </div>
           </div> -->
-        </div>
+        <!-- </div> -->
       </div>
     </nav>
   </div>
--- FrontEnd/src/views/FindApt.vue
+++ FrontEnd/src/views/FindApt.vue
@@ -1,6 +1,63 @@
 <template>
     <div>
-        <div class="blank"/>
+        <div class="bg-image">
+            <div class="mask">
+                <div class="d-flex justify-content-center align-items-center h-100 pt-5" style="flex-direction: column;">
+                    <div>
+                        <h1 class="text-white mb-5">Find Your Apartment</h1>
+                    </div>
+                    <div>
+                        <ol class="breadcrumb justify-content-center mb-0">
+                            <div class="row col-md justify-content-center mb-2">
+                                <div class="form-group col-md">
+                                    <select class="form-select bg-white text-black" id="sido" style="width:150px">
+                                        <option value="all">도/광역시</option>
+                                        <option value="11">서울시</option>
+                                        <option value="41">경기도</option>
+                                        <option value="28">인천광역시</option>
+                                        <option value="26">부산광역시</option>
+                                        <option value="30">대전광역시</option>
+                                        <option value="27">대구광역시</option>
+                                        <option value="31">울산광역시</option>
+                                        <option value="36">세종시</option>
+                                        <option value="29">광주광역시</option>
+                                        <option value="42">강원도</option>
+                                        <option value="43">충청북도</option>
+                                        <option value="47">경상북도</option>
+                                        <option value="48">경상남도</option>
+                                        <option value="45">전라북도</option>
+                                        <option value="46">전라남도</option>
+                                        <option value="50">제주도</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md">
+                                    <select class="form-select bg-white text-black" id="gugun" style="width:150px">
+                                        <option value="all">구/군</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md">
+                                    <select class="form-select bg-white text-black" id="dong" style="width:150px">
+                                        <option value="all">동</option>
+                                        <!-- <c:forEach var="dong" items="${donglist}">
+                                            <option value="${dong.dongcode}"> ${dong.dongname}</option>
+                                        </c:forEach> -->
+                                    </select>
+                                </div>
+                                <button type="button" id="list-btn" class="btn btn-outline-warning ms-3 border-2 opacity-75" style="width: 210px">
+                                    아파트매매정보가져오기
+                                </button>
+                                &nbsp;&nbsp;&nbsp;
+                                <!-- <c:if test="${sessionScope.userInfo.id ne null }">
+                                <button type="button" id="btnAddBookmark" class="btn btn-outline-primary" style="width: 210px">
+                                    관심 지역 등록
+                                </button>
+                                </c:if> -->
+                            </div>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="aptBody">
             <div class="aptTitle">
             </div>
@@ -30,19 +87,30 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Black+Han+Sans&family=Dongle&family=Jua&family=Noto+Sans+KR&display=swap');
-.blank {
-    height: 400px;
-    /* background-color: #212429; */
-    background-image: url("@/assets/img/carousel/carousel-1.jpg");
+@import url('https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Dongle&family=Jua&family=Noto+Sans+KR&display=swap');
+.bg-image{
+    height: 500px;
+    background-image: url('@/assets/img/findAptTitle.jpg'); 
+}
+.mask{
+    background-color: rgba(0, 0, 0, 0.6);
+    height: 500px;
+}
+.mask h1{
+    font-family: 'Black Han Sans', sans-serif;
+    color:white;
+    font-size: 50px;
+    transform: skew(-0.1deg);
+}
+#list-btn{
+    font-family: 'Jua', sans-serif;
+    font-size: 17px;
+}
+ol{
+    font-family: 'Noto Sans KR', sans-serif;
 }
 .aptBody {
     height: 1000px;
     background: #F9F9F9;
-}
-.aptTitle h1{
-    font-family: 'Do Hyeon', sans-serif;
-    color:black;
-    font-size: 70px;
-    transform: skew(-0.1deg);
 }
 </style>
