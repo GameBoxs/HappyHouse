ca00dc4783c3eb8af3abee9b1b4ab9369c5803f9
김지훈 <ji_hun23@naver.com>
2022-11-16 15:17:23 +0900

    feat(FE): Initial FE

---
+++ FrontEnd/src/views/FindApt.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="blank"/>
+        <div class="aptBody">
+            <div class="aptTitle">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WhereismyhomeVueFindApt',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Black+Han+Sans&family=Dongle&family=Jua&family=Noto+Sans+KR&display=swap');
+.blank {
+    height: 400px;
+    /* background-color: #212429; */
+    background-image: url("@/assets/img/carousel/carousel-1.jpg");
+}
+.aptBody {
+    height: 1000px;
+    background: #F9F9F9;
+}
+.aptTitle h1{
+    font-family: 'Do Hyeon', sans-serif;
+    color:black;
+    font-size: 70px;
+    transform: skew(-0.1deg);
+}
+</style>
