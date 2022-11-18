2b24fe1ef7073164cd6f053f06e432ca384cedd4
limecats0331 <limecats0331@gmail.com>
2022-11-18 16:38:17 +0900

    fix(BE): 동이 없는 구/군 정보 제거

--- BackEnd/src/main/java/com/ssafy/happyhouse/repository/DongCodeRepository.java
+++ BackEnd/src/main/java/com/ssafy/happyhouse/repository/DongCodeRepository.java
@@ -11,9 +11,10 @@ import java.util.Optional;
 
 @Repository
 public interface DongCodeRepository extends JpaRepository<DongCode, String> {
-    @Query("select distinct d.gugunName from DongCode d where d.sidoName like :sido% and d.gugunName is not null")
+    @Query("select distinct d.gugunName from DongCode d where d.sidoName like :sido% and d.gugunName is not null and d.dongName is not null")
     List<String> findGugunNames(@Param("sido") String sido);
 
+    // TODO 시/도 중에 동 이름이 없는 데이터들은 필터링해서 보여주지 않도록한다.
     @Query("select distinct d.dongName from DongCode d where d.sidoName = :sido and d.gugunName = :gugun and d.dongName is not null")
     List<String> findDongName(@Param("sido") String sido, @Param("gugun") String gugun);
 
