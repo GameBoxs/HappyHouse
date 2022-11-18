e34b219a983def12e924f443d2a11cef3e4faaa0
limecats0331 <limecats0331@gmail.com>
2022-11-18 17:27:33 +0900

    feat(BE): CORS 설정 추가

--- BackEnd/src/main/java/com/ssafy/happyhouse/config/WebConfig.java
+++ BackEnd/src/main/java/com/ssafy/happyhouse/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.happyhouse.config;
 
 import com.ssafy.happyhouse.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LogInterceptor())
                 .order(1)
                 .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
+                .allowedHeaders("headers")
+                .maxAge(3000);
     }
 }
